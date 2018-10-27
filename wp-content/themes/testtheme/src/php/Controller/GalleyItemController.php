dd4251a45826f9b2c4458ebd61ec05879357dc75
Diana Delisle <diana.delisle87@gmail.com>
2018-10-27 14:50:58 +0000

    first commit

---
+++ wp-content/themes/testtheme/src/php/Controller/GalleyItemController.php
@@ -0,0 +1,17 @@
+8<?php
+namespace php\Controllers;
+
+class GalleryItemController{
+    
+    public function GalleryItem(){
+        
+        $args = [];
+
+        //any query or backend operation you want to do
+        $args['test'] = 'test';
+
+        return $args;
+    }
+    
+}
+?>
